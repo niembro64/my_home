a489496c89e8f39b2734fbf3c92094bf885804e5
Eric Niemeyer <niemeyer.eric@gmail.com>
2023-04-17 00:38:18 -0400

    touching lr

--- src/phaser/helpersPhaser/sprite.ts
+++ src/phaser/helpersPhaser/sprite.ts
@@ -4,6 +4,15 @@ import GameScene from '../GameScene';
 export const updateSpriteFlip = (player: Player, game: GameScene): void => {
   const k = player;
   const width = 150;
+
+  if (k.sprite.body.touching.left) {
+    k.sprite.flipX = true;
+    return;
+  } else if (k.sprite.body.touching.right) {
+    k.sprite.flipX = false;
+    return;
+  }
+
   if (k.sprite.body.velocity.x < -width) {
     k.sprite.flipX = true;
   } else if (k.sprite.body.velocity.x > width) {
